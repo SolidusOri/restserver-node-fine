367c564316faadcdd6da5b83f4174bb0305ab00c
Robinson <robinsonallendes@hotmail.com>
2018-12-20 16:17:52 -0300

    Comentarios en el config.js

--- server/config/config.js
+++ server/config/config.js
@@ -6,15 +6,18 @@ process.env.PORT = process.env.PORT || 3000;
 
 
 //Entorno
-//process.env.NODE_ENV es una variable de entonrno en heroku
+//process.env.NODE_ENV es una variable de entonrno en heroku, si existe esta variable estamos en produccion, si no en dessarrllo
 process.env.NODE_ENV = process.env.NODE_ENV || 'desarrollo';
 
 //base de datos
-let urlDB;
+let urlDB; //cadena de conexion
+
+//dependiendo del environment la app usará una cadena de conexion o la otra
 if (process.env.NODE_ENV === 'desarrollo') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     //process.env.MONGO_URL, variable de entorno creada en heroku
     urlDB = process.env.MONGO_URL;
 }
+
 process.env.URLDB = urlDB;
